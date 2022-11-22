2cdb857a9353fc6655eb4fd39b92b6ba87830f82
LongBOTT <93486974+LongBOTT@users.noreply.github.com>
2022-11-22 10:14:20 +0700

    search + filter

--- js/script.js
+++ js/script.js
@@ -32,6 +32,72 @@ function nextBanner() {
     next = 1;
     runBanner();
 }
+function removeVietnameseTones(str) {
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a"); 
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e"); 
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g,"i"); 
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g,"o"); 
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g,"u"); 
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g,"y"); 
+    str = str.replace(/đ/g,"d");
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+    str = str.replace(/Đ/g, "D");
+    // Some system encode vietnamese combining accent as individual utf-8 characters
+    // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
+    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+    str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+    // Remove extra spaces
+    // Bỏ các khoảng trắng liền nhau
+    str = str.replace(/ + /g," ");
+    str = str.trim();
+    // Remove punctuations
+    // Bỏ dấu câu, kí tự đặc biệt
+    // str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g," ");
+    return str;
+}
+function search(){
+    var name=document.getElementById("searchPro").value;
+    var category = JSON.parse(localStorage.getItem('category'));
+    document.getElementById("headline").innerHTML = "<h3>Sản phẩm</h3>";
+    books=[];
+    //kiem tra tu khoa tim kiem co dau hay khong dau
+    if (name.toLowerCase().includes(removeVietnameseTones(name).toLowerCase())){
+        name=removeVietnameseTones(name).toLowerCase();
+        for (var i = 0; i < category.length; i++) {
+            for (var j = 0; j < category[i].listcategory.length; j++) {
+                for (var k = 0; k < category[i].listcategory[j].books.length; k++) {
+                    var namebook=removeVietnameseTones(category[i].listcategory[j].books[k].name).toLowerCase()
+                    if (namebook.includes(name)) {
+                        books.push(category[i].listcategory[j].books[k]);
+                    }
+                }
+            }
+        }
+    }else{
+        name=name.toLowerCase();
+        for (var i = 0; i < category.length; i++) {
+            for (var j = 0; j < category[i].listcategory.length; j++) {
+                for (var k = 0; k < category[i].listcategory[j].books.length; k++) {
+                    var namebook=category[i].listcategory[j].books[k].name.toLowerCase()
+                    if (namebook.includes(name)) {
+                        books.push(category[i].listcategory[j].books[k]);
+                    }
+                }
+            }
+        }
+    }
+    currenPage = 1;
+    totalpage = Math.ceil(books.length / perPage)
+    getCurrentPage(currenPage, books);
+    renderProduct(books);
+    renderListPage();
+    changePage(books);
+}
 function showBtFilter(){
     document.getElementById("filterAndSort").setAttribute("style","width: 265px");
 }
@@ -79,8 +145,6 @@ function sortLow_HighPrice(){
     var str = window.location.href;
     if (filter){
         books=filteredBooks;
-    }else{
-        books=initialBooks;
     }
     for (var i = 0; i < books.length-1; i++){
         for (var j = i+1; j < books.length; j++){
@@ -103,14 +167,14 @@ function sortLow_HighPrice(){
     }else{
         renderBestseller(books);
     }
-    books=initialBooks;
+    if (filter){
+        books=initialBooks;
+    }
 }
 function sortHigh_LowPrice(){
     var str = window.location.href;
     if (filter){
         books=filteredBooks;
-    }else{
-        books=initialBooks;
     }
     for (var i = 0; i < books.length-1; i++){
         for (var j = i+1; j < books.length; j++){
@@ -133,7 +197,9 @@ function sortHigh_LowPrice(){
     }else{
         renderBestseller(books);
     }
-    books=initialBooks;
+    if (filter){
+        books=initialBooks;
+    }
 }
 function filterOption1(){
     var str = window.location.href;
@@ -313,6 +379,34 @@ function showLogin() {
 function showSignUp() {
     document.getElementById("login").style.display = "none";
     document.getElementById("signup").style.display = "block";
+}
+function createDevice() {
+    if (JSON.parse(localStorage.getItem('category')) != null)
+        return;
+    const devices = [
+        {id:1, name:"Dụng cụ học sinh", quantity:0, listcategory: [
+            {id:"BV", name:"Bút viết", quantity:0, devices:[]},
+            {id:"T", name:"Tẩy", quantity:0, devices:[]},
+            {id:"TK", name:"Thước kẻ", quantity:0, devices:[]},
+            {id:"MTCT", name:"Máy tính cầm tay", quantity:0, devices:[]},
+        ]},
+        {id:2, name:"Dụng cụ văn phòng", quantity:0, listcategory: [
+            {id:"NOTE", name:"Sổ tay/Ghi chú", quantity:0, devices:[]},
+            {id:"TUI", name:"Túi/Bìa đụng hồ sơ", quantity:0, devices:[]},
+            {id:"CD", name:"Cắt/Dán", quantity:0, devices:[]},
+            {id:"KHVP", name:"Khác", quantity:0, devices:[]},
+        ]},
+        {id:3, name:"Dụng cụ vẽ", quantity:0, listcategory: [
+            {id:"BV", name:"Bảng vẽ", quantity:0, devices:[]},
+            {id:"BUV", name:"Bút vẽ", quantity:0, devices:[]},
+            {id:"BUM", name:"Bút màu", quantity:0, devices:[]},
+            {id:"MN", name:"Màu nước", quantity:0, devices:[]},
+            {id:"CV", name:"Cọ vẽ", quantity:0, devices:[]},
+            {id:"KHVE", name:"Khác", quantity:0, devices:[]},
+        ]},
+        {id:4, name:"Khác", quantity:0, listcategory: []},
+    ]
+    
 }
 function createBook() {
     if (JSON.parse(localStorage.getItem('category')) != null)
